6099687c6f6fa92e00d5d153ac9537e872bb90d8
user <user@1008-044>
2026-05-20 07:22:20 +0800

    加入專案檔案。

---
+++ s1111425_劉法宏/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace s1111425_劉法宏
+{
+    partial class frmBooks
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmBooks));
+            this.imgL = new System.Windows.Forms.ImageList(this.components);
+            this.imgS = new System.Windows.Forms.ImageList(this.components);
+            this.lvwBooks = new System.Windows.Forms.ListView();
+            this.cmbView = new System.Windows.Forms.ComboBox();
+            this.grpBorrow = new System.Windows.Forms.GroupBox();
+            this.grpView = new System.Windows.Forms.GroupBox();
+            this.pnlTools = new System.Windows.Forms.Panel();
+            this.lstBorrow = new System.Windows.Forms.ListBox();
+            this.grpBorrow.SuspendLayout();
+            this.grpView.SuspendLayout();
+            this.pnlTools.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // imgL
+            // 
+            this.imgL.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgL.ImageStream")));
+            this.imgL.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgL.Images.SetKeyName(0, "Book1.bmp");
+            this.imgL.Images.SetKeyName(1, "Book2.bmp");
+            this.imgL.Images.SetKeyName(2, "Book3.bmp");
+            this.imgL.Images.SetKeyName(3, "Book4.bmp");
+            this.imgL.Images.SetKeyName(4, "Book5.bmp");
+            this.imgL.Images.SetKeyName(5, "Book6.bmp");
+            this.imgL.Images.SetKeyName(6, "Book7.bmp");
+            this.imgL.Images.SetKeyName(7, "Book8.bmp");
+            this.imgL.Images.SetKeyName(8, "Book1.bmp");
+            // 
+            // imgS
+            // 
+            this.imgS.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgS.ImageStream")));
+            this.imgS.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgS.Images.SetKeyName(0, "Book1.bmp");
+            this.imgS.Images.SetKeyName(1, "Book2.bmp");
+            this.imgS.Images.SetKeyName(2, "Book3.bmp");
+            this.imgS.Images.SetKeyName(3, "Book4.bmp");
+            this.imgS.Images.SetKeyName(4, "Book5.bmp");
+            this.imgS.Images.SetKeyName(5, "Book6.bmp");
+            this.imgS.Images.SetKeyName(6, "Book7.bmp");
+            this.imgS.Images.SetKeyName(7, "Book8.bmp");
+            // 
+            // lvwBooks
+            // 
+            this.lvwBooks.HideSelection = false;
+            this.lvwBooks.LargeImageList = this.imgL;
+            this.lvwBooks.Location = new System.Drawing.Point(12, 12);
+            this.lvwBooks.Name = "lvwBooks";
+            this.lvwBooks.Size = new System.Drawing.Size(394, 426);
+            this.lvwBooks.SmallImageList = this.imgS;
+            this.lvwBooks.TabIndex = 0;
+            this.lvwBooks.UseCompatibleStateImageBehavior = false;
+            this.lvwBooks.ItemActivate += new System.EventHandler(this.lvwBooks_ItemActivate);
+            this.lvwBooks.SelectedIndexChanged += new System.EventHandler(this.lvwBooks_SelectedIndexChanged);
+            // 
+            // cmbView
+            // 
+            this.cmbView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmbView.FormattingEnabled = true;
+            this.cmbView.Location = new System.Drawing.Point(19, 53);
+            this.cmbView.Name = "cmbView";
+            this.cmbView.Size = new System.Drawing.Size(357, 20);
+            this.cmbView.TabIndex = 1;
+            this.cmbView.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // grpBorrow
+            // 
+            this.grpBorrow.Controls.Add(this.lstBorrow);
+            this.grpBorrow.Location = new System.Drawing.Point(3, 143);
+            this.grpBorrow.Name = "grpBorrow";
+            this.grpBorrow.Size = new System.Drawing.Size(382, 295);
+            this.grpBorrow.TabIndex = 2;
+            this.grpBorrow.TabStop = false;
+            this.grpBorrow.Text = "借書清單";
+            // 
+            // grpView
+            // 
+            this.grpView.Controls.Add(this.cmbView);
+            this.grpView.Dock = System.Windows.Forms.DockStyle.Top;
+            this.grpView.Location = new System.Drawing.Point(0, 0);
+            this.grpView.Name = "grpView";
+            this.grpView.Size = new System.Drawing.Size(388, 134);
+            this.grpView.TabIndex = 0;
+            this.grpView.TabStop = false;
+            this.grpView.Text = "檢視方式";
+            // 
+            // pnlTools
+            // 
+            this.pnlTools.Controls.Add(this.grpView);
+            this.pnlTools.Controls.Add(this.grpBorrow);
+            this.pnlTools.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pnlTools.Location = new System.Drawing.Point(412, 0);
+            this.pnlTools.Name = "pnlTools";
+            this.pnlTools.Size = new System.Drawing.Size(388, 450);
+            this.pnlTools.TabIndex = 0;
+            // 
+            // lstBorrow
+            // 
+            this.lstBorrow.FormattingEnabled = true;
+            this.lstBorrow.ItemHeight = 12;
+            this.lstBorrow.Location = new System.Drawing.Point(6, 21);
+            this.lstBorrow.Name = "lstBorrow";
+            this.lstBorrow.Size = new System.Drawing.Size(367, 256);
+            this.lstBorrow.TabIndex = 2;
+            // 
+            // frmBooks
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pnlTools);
+            this.Controls.Add(this.lvwBooks);
+            this.Name = "frmBooks";
+            this.Text = "圖書管理";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.grpBorrow.ResumeLayout(false);
+            this.grpView.ResumeLayout(false);
+            this.pnlTools.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imgL;
+        private System.Windows.Forms.ImageList imgS;
+        private System.Windows.Forms.ListView lvwBooks;
+        private System.Windows.Forms.ComboBox cmbView;
+        private System.Windows.Forms.GroupBox grpBorrow;
+        private System.Windows.Forms.GroupBox grpView;
+        private System.Windows.Forms.Panel pnlTools;
+        private System.Windows.Forms.ListBox lstBorrow;
+    }
+}
+
